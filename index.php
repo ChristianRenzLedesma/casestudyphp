8781944987b2e1432f2f8b07cea1a6a33dcdc8ab
ChristianRenzLedesma <155465499+ChristianRenzLedesma@users.noreply.github.com>
2024-03-26 14:01:45 +0800

    new

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Madimi+One&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <style>
+        <?php include "style.css"?>
+    </style>
+</head>
+
+<body>
+    <?php include "nav.php"?>
+    <h1 class="title">Tara na sa Quezon</h1>
+    <p class="sub">We are proud to share with you the astonishing beauty of <strong>Patnanungan Quezon.</strong> <br>Come visit and explore
+        the natural beauty of our Island!</p>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="about.php">ABOUT</a></li>
+        <li><a href="">PLACES</a></li>
+        <li><a href="">INFO</a></li>
+    </ul>
+</nav>
